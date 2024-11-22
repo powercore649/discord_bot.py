176d539f16614142243e6db1aff906ea9e47596a
powercore649 <126120353+powercore649@users.noreply.github.com>
2024-11-21 22:44:57 -0500

    Create requirements.cmd

---
+++ requirements.cmd
@@ -0,0 +1,14 @@
+@echo off
+
+:start
+cls
+
+pip install discord
+pip install selenium
+pip install undetected_chromedriver
+pip install googletranslate
+pip install g4f
+pip install dotenv
+
+pause
+exit
